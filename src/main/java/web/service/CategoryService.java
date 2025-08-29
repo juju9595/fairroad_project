24c348804ada230fae7912989e7fcd6afa1b8700
LEE-GYEO-RE <rufp9891@gmail.com>
2025-08-29 18:01:16 +0900

    02_이겨레_Service_서비스 어노테이션 생성

--- src/main/java/web/service/CategoryService.java
+++ src/main/java/web/service/CategoryService.java
@@ -1,4 +1,10 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.model.dao.CategoryDao;
+
+@Service
 public class CategoryService {
+    @Autowired CategoryDao categoryDao;
 }
