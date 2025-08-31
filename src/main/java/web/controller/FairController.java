389245c8ef4a1a8817ea6219b9da4e8c4413da13
LEE-GYEO-RE <rufp9891@gmail.com>
2025-08-31 22:49:47 +0900

    11_이겨레_FairDao_지역별 박람회 조회 완료

--- src/main/java/web/controller/FairController.java
+++ src/main/java/web/controller/FairController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import web.model.dto.FairCountDto;
+import web.model.dto.FairRegionDto;
 import web.service.FairService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +25,13 @@ public class FairController { // class start
     @GetMapping("/fcount")
     public List<FairCountDto> fcountList(){
         List<FairCountDto> result = fairService.fcountList();
+        return result;
+    } // func e
+
+    // 지역별 박람회 조회
+    @GetMapping("/fregion")
+    public Map<String , List<FairRegionDto>> fregionList(){
+        Map<String , List<FairRegionDto>> result = fairService.fregionList();
         return result;
     } // func e
 
--- src/main/java/web/model/dao/FairDao.java
+++ src/main/java/web/model/dao/FairDao.java
@@ -2,6 +2,7 @@ package web.model.dao;
 
 import org.springframework.stereotype.Repository;
 import web.model.dto.FairCountDto;
+import web.model.dto.FairDto;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,6 +19,7 @@ public class FairDao extends Dao{
             String sql = " select fno , fname , fcount from fair order by fcount desc ";
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
+
             while (rs.next()){
                 FairCountDto dto = new FairCountDto();
                 dto.setFno(rs.getInt("fno"));
@@ -31,7 +33,27 @@ public class FairDao extends Dao{
         return list;
     } // func e
 
-    // 지역별 박람회 조회
+    // 지역별 그룹핑용 전체 박람회 조회
+    public List<FairDto> selectAllFairs(){
+        List<FairDto> list = new ArrayList<>();
+        try {
+            String sql = " select fno , fname , fplace from fair order by fplace , fno ";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+                FairDto dto = new FairDto();
+                dto.setFno(rs.getInt("fno"));
+                dto.setFname(rs.getString("fname"));
+                dto.setFplace(rs.getString("fplace"));
+                list.add(dto);
+            }
+        }catch (Exception e ){
+            System.out.println(e);
+        }
+        return list;
+    } // func e
+
 
     // 추천(알고리즘) 박람회 조회
 
