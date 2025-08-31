dd6f04f599b3d6a0f75f9e392a9b6b97211d2c88
leechanyeong <yahn10@naver.com>
2025-08-31 15:24:45 +0900

    10_이찬영_fairWrite,fairPrint_등록완료,전체조회 에서 페이징 ,검색 기능 구현 진행 해야함

--- src/main/java/web/service/FairService.java
+++ src/main/java/web/service/FairService.java
@@ -3,9 +3,26 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.model.dao.FairDao;
+import web.model.dto.FairDto;
+import web.model.dto.PageDto;
+
+import java.util.List;
 
 @Service
 public class FairService {
     @Autowired FairDao fairDao;
+
+    //박람회 등록
+    public int fairWrite(FairDto fairDto){
+        int result = fairDao.fairWrite(fairDto);
+        return result;
+    }//func end
+
+    //박람회 전체 조회
+    public List<FairDto> fairPrint(){
+        return fairDao.fairPrint();
+    }//func end
+
+
 
 }//class end
