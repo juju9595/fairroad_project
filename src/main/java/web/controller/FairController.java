802520fb030b05d30f2a1965cdce906fdd37c677
leechanyeong <yahn10@naver.com>
2025-08-31 15:25:34 +0900

    12_이찬영_fairWrite,fairPrint_talend 에서 확인

--- src/main/java/web/controller/FairController.java
+++ src/main/java/web/controller/FairController.java
@@ -2,13 +2,32 @@ package web.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import web.model.dto.FairDto;
 import web.service.FairService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/fair")
 public class FairController { // class start
-
-
     private final FairService fairService;
+
+    //박람회 등록
+    @PostMapping("/write")
+    public int fairWrite(@RequestBody FairDto fairDto){
+        System.out.println("FairController.fairWrite");
+        System.out.println("fairDto = " + fairDto);
+        int result = fairService.fairWrite(fairDto);
+        return result;
+    }//func end
+
+    //박람회 전체
+    @GetMapping("/print")
+    public List<FairDto> fairPrint(){
+        return fairService.fairPrint();
+    }//func end
+
+
 } // class end
