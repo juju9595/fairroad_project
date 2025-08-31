34319b36ea156a52f04686c3645c0698e9276cef
LEE-GYEO-RE <rufp9891@gmail.com>
2025-08-31 21:07:39 +0900

    07_이겨레_WishListDao_회원별 즐겨찾기 목록 조회 완료

---
+++ src/main/java/web/controller/WishListController.java
@@ -0,0 +1,27 @@
+package web.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import web.model.dto.MemberWishListDto;
+import web.service.WishListService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/visitlog")
+public class WishListController { // class start
+    @Autowired
+    private WishListService wishlistService;
+
+    // 회원별 즐겨찾기 목록 조회
+    @GetMapping("/member")
+    public MemberWishListDto memberWishList(@RequestParam int mno){
+        MemberWishListDto result = wishlistService.memberWishList(mno);
+        return result;
+    } // func e
+
+} // class end
--- src/main/java/web/model/dao/WishListDao.java
+++ src/main/java/web/model/dao/WishListDao.java
@@ -1,8 +1,31 @@
 package web.model.dao;
 
 import org.springframework.stereotype.Repository;
+import web.model.dto.WishListDto;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class WishListDao extends Dao{
 
-}
+    // 회원별 즐겨찾기 목록 조회
+    public List<WishListDto> memberWishList(int mno){
+        List<WishListDto> list = new ArrayList<>();
+        String sql = "select f.fno , f.fname from wishlist w join fair f on w.fno = f.fno where w.mno = ? ";
+        try (PreparedStatement ps = conn.prepareStatement(sql)){ // PreparedStatement 블록 끝나면 자동 close
+            ps.setInt(1, mno);
+            try (ResultSet rs = ps.executeQuery()){ // ResultSet 블록 끝나면 자동 close
+                while (rs.next()){
+                    WishListDto dto = new WishListDto(rs.getInt("fno") , rs.getString("fname"));
+                    list.add(dto);
+                }
+            }
+        }catch (Exception e ){
+            System.out.println(e);
+        }
+        return list;
+    } // func e
+} // class e
