aa8c5b86139a2da8860bb46be4d339e040f1566b
LEE-GYEO-RE <rufp9891@gmail.com>
2025-08-31 20:12:09 +0900

    04_이겨레_WishListDto_즐겨찾기 Dto 멤버변수 수정

--- src/main/java/web/model/dto/WishListDto.java
+++ src/main/java/web/model/dto/WishListDto.java
@@ -7,10 +7,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class WishlistDto {
+public class WishListDto { // 즐겨찾기 된 박람회 리스트 Dto
 
-    private int wno;    // 즐겨찾기 번호
-    private int mno;    // 회원 번호(FK)
-    private int fno;    // 박람회 번호(FK)
+    private int fno;       // 박람회 번호
+    private String fname;  // 박람회 이름
 
 }//class
