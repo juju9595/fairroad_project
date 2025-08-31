43d5652ca70014c15da93940d636312ca4d6c215
LEE-GYEO-RE <rufp9891@gmail.com>
2025-09-01 01:51:47 +0900

    18_이겨레_CategoryRecommendStrategy_Stream API 함수 주석처리

--- src/main/java/web/service/strategy/CategoryRecommendStrategy.java
+++ src/main/java/web/service/strategy/CategoryRecommendStrategy.java
@@ -24,19 +24,21 @@ public class CategoryRecommendStrategy implements RecommendStrategy {
         List<Integer> visitedFno = visitLogDao.getVisitFnoByMember(mno);
 
         // fno -> FairDto 가져오기
-        Map<Integer , Integer> categoryCount = new HashMap<>();
-        for (int fno : visitedFno){
+        Map<Integer , Integer> categoryCount = new HashMap<>(); // key = 카테고리번호(cno) , value = 방문횟수
+        for (int fno : visitedFno){                             // 회원이 방문한 fno 목록 순회 하면서 각 fno의 카테고리 가져와서 카운트 저장
             FairDto fair = fairDao.getFairbyFno(fno);
             if(fair != null){
-                categoryCount.put(fair.getCno() , categoryCount.put(fair.getCno() , 0 ) + 1);
+                categoryCount.put(fair.getCno() , categoryCount.put(fair.getCno() , 0 ) + 1);   // 해당 카테고리 값 없으면 0반환
             }
         } // for e
 
         // 방문 빈도 가장 높은 카테고리 선택
         int topCategory = categoryCount.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .orElse(0);
+                // categoryCount.entrySet() : Map의 모든 쌍을 Set로 반환 = 배열화
+                // .stream() : entrySet을 stream으로 변환 함수형 처리 가능
+                .max(Map.Entry.comparingByValue())      // value(방문 횟수) 가장 큰 요소 선택
+                .map(Map.Entry::getKey)                 // 선택된 Entry에서 key만 꺼냄!! -> 카테고리 번호
+                .orElse(0);                     // Map 비어있고 max 없으면 기본값 0 반환
 
         // 해당 카테고리 박람회 조회
         return fairDao.getFairsByCategory(topCategory);
