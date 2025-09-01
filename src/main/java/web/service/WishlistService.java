77f53394b8bde9e4aa49894fc67083235fd02ffe
LEE-GYEO-RE <rufp9891@gmail.com>
2025-09-01 10:32:43 +0900

    21_이겨레_VisitLogService_클래스명 카멜표기법으로 수정

--- src/main/java/web/service/WishlistService.java
+++ src/main/java/web/service/WishlistService.java
@@ -3,9 +3,21 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.model.dao.WishListDao;
+import web.model.dto.MemberWishListDto;
+import web.model.dto.WishListDto;
+
+import java.util.List;
 
 @Service
-public class WishlistService {
+public class WishListService {
     @Autowired
     WishListDao wishlistDao;
-}
+
+    // 회원별 즐겨찾기 목록 조회
+    public MemberWishListDto memberWishList(int mno){
+        List<WishListDto> wishList = wishlistDao.memberWishList(mno);
+        MemberWishListDto result = new MemberWishListDto(mno , wishList);
+        return result;
+    } // func e
+
+} // class e
