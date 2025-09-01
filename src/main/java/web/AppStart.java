675bc7e6e98f0a0f93bb2ee0b4a61adc208c0a86
LEE-GYEO-RE <rufp9891@gmail.com>
2025-09-01 23:00:16 +0900

    32_이겨레_AppStart_@EnableAsync 비동기 처리 활성화 추가

--- src/main/java/web/AppStart.java
+++ src/main/java/web/AppStart.java
@@ -3,8 +3,10 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync // 비동기 처리 활성화
 public class AppStart { // class start
     public static void main(String[] args) { // main start
 //12349
