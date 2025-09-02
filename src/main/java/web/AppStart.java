5de2e26646591a4b913f552b4aceb9f506bff006
juju9595 <eunju220926@gmail.com>
2025-09-02 18:08:09 +0900

    04_정은주_member 구현중

--- src/main/java/web/AppStart.java
+++ src/main/java/web/AppStart.java
@@ -9,8 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync // 비동기 처리 활성화
 public class AppStart { // class start
     public static void main(String[] args) { // main start
-//12349
         SpringApplication.run( AppStart.class );
-
     } // main end
-} // class end 1234567678912
+} // class end
--- src/main/java/web/model/dao/MemberDao.java
+++ src/main/java/web/model/dao/MemberDao.java
@@ -1,16 +1,23 @@
 package web.model.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.dto.MembersDto;
+import web.model.dto.WishListDto;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class MemberDao extends Dao{
 
-    //회원가입
+    @Autowired private WishListDao wishListDao;
+
+    // [1] 회원가입
     public int signUp(MembersDto membersDto){
         try{
             String sql = "insert into members (mid, mpwd, mname, mbirth, mphone, memail, maddress) values(?,?,?,?,?,?,?)";
@@ -36,7 +43,7 @@ public class MemberDao extends Dao{
     }//func e
 //-----------------------------------------------------------------------------------------//
 
-    //로그인
+    // [2] 로그인
     public int login(MembersDto membersDto){
         try{
             String sql = "select *from members where mid =? and mpwd =?";
@@ -51,7 +58,58 @@ public class MemberDao extends Dao{
         }catch (Exception e){
             System.out.println();
         }return 0;
-    }//func e
-    //-----------------------------------------------------------------------------------------//
-    //
+    }//func
+
+
+    // -----------------------------------------------------------------------------------------//
+
+    // [4] 연락처 수정
+    public boolean phoneUpdate(MembersDto membersDto){
+        try{
+            String sql = "update members set mphone=? where mno =?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, membersDto.getMphone());
+            int count = ps.executeUpdate();
+            return count == 1;
+        } catch (Exception e) {
+            System.out.println(e);
+        }return false;
+    }
+
+
+// -----------------------------------------------------------------------------------------//
+
+    // [5] 비밀번호 수정
+    public boolean pwdUpdate(int mno, Map<String, String> map){
+        try{
+            String sql = "update members set mpwd =? where mno=? and mpwd =?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, map.get("newpwd"));
+            ps.setInt(2, mno);
+            ps.setString(3, map.get("oldpwd"));
+            int count = ps.executeUpdate();
+            return count == 1;
+        }catch (Exception e){
+            System.out.println(e);
+        }return false;
+    }
+
+
+// -----------------------------------------------------------------------------------------//
+
+    // [6] 즐겨찾기 목록
+    public List<WishListDto> wishList(){
+        List<WishListDto> list = new ArrayList<>();
+        try{String sql = "select *from wishlist";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                WishListDto wishListDto = new WishListDto();
+                wishListDto.setFno(rs.getInt("fno"));
+                wishListDto.setFname(rs.getString("fname"));
+                list.add(wishListDto);
+            }
+        }catch (Exception e) {System.out.println();}
+        return list;
+    }
 }//class e
