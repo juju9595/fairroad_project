6e1b2638a76281379936e400719beea68bcea613
l-yeong <yahn10@naver.com>
2025-09-02 17:44:16 +0900

    33_이찬영_fileUpload_talend 에서 업로드 정상 후 upload 패키지 사진 업로드 확인

--- src/main/java/web/service/FileService.java
+++ src/main/java/web/service/FileService.java
@@ -19,18 +19,24 @@ public class FileService {
     public String fileUpload(MultipartFile multipartFile){
         System.out.println("FileService.fileUpload");
         System.out.println("multipartFile = " + multipartFile);
+
         String uuid = UUID.randomUUID().toString(); //파일명 UUID
         String fimg = uuid +"_"+multipartFile.getOriginalFilename().replaceAll("_","-");//파일명 ("기본문자"+"새로운문자")
         String uploadFilePath = uploadPath + fimg; // 업로드 경로와 파일명 합치기
 
         File pathFile = new File(uploadPath); // 업로드한 경로가 upload 폴더가 존재 하지않으면 폴더 생성
+        System.out.println("pathFile"+pathFile);
+
         if(!pathFile.exists()){
             pathFile.mkdir();
         }//if end
         File uploadFile = new File(uploadFilePath); //업로드 할 경로를 file 클래스 생성
+
+        System.out.println("uploadFile"+uploadFile);
+
         try{
             multipartFile.transferTo(uploadFile); // multipartFile(업로드된 파일)
-        } catch (IOException e) {System.out.println(e);; return null;}//catch end
+        } catch (IOException e) {System.out.println(e); return null;}//catch end
         return fimg;
     }//func end
 }//class end
