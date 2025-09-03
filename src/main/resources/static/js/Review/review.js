87b4c5d2c98e1ff6a4e085c1e9d80c72c33ac207
yj08-80 <{email}>
2025-09-03 18:02:25 +0900

    13_정유진_review.jsp수정중

--- src/main/resources/static/js/Review/review.js
+++ src/main/resources/static/js/Review/review.js
@@ -1,2 +1,32 @@
-console.log( "review.js open" );
+console.log("review.js open");
+
+const findAllReview = async () => {
+  try {
+    const res = await fetch('/fair/review/print'); // 전체 조회 API
+    const data = await res.json();
+
+    const reviewTbody = document.querySelector('.reviewTbody');
+    let html = '';
+    data.forEach(review => {
+      html += `
+        <tr>
+          <td>${review.rno}</td>
+          <td>
+            <a href="/reviewDetail.jsp?rno=${review.rno}">
+              ${review.rtitle}
+            </a>
+          </td>
+        </tr>`;
+    });
+
+    reviewTbody.innerHTML = html;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// 페이지 로드시 자동 실행
+findAllReview();
+
+
 
