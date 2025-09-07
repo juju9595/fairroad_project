87fa13c6ad5a309c538f8cb42c8982f70e046e88
juju9595 <git>
2025-09-07 14:29:32 +0900

    10_정은주_jsp,js_구현/수정

--- src/main/resources/static/js/Members/wishlist.js
+++ src/main/resources/static/js/Members/wishlist.js
@@ -10,7 +10,14 @@ const wishlistFind = async() => { console.log('wishlistFind. exe')
     let html = '';
     for(let i = 0; i<data.length; i++){
         const wishlist = data[i];
-        html += `${wishlist.fname}`
+        html += `
+        <tr>
+          <td style="text-align:center;">
+            <input type="checkbox" class="wish" value="${wishlist.fno}">
+          </td>
+          <td>${wishlist.fname}</td>
+        </tr>
+      `;
     }
     wishlistBox.innerHTML = html;
     }catch(error){
@@ -21,5 +28,36 @@ const wishlistFind = async() => { console.log('wishlistFind. exe')
 } 
 wishlistFind();
 
+const wishlistRenderRows = async () => {
+  const res = await fetch('/member/wishlist');
+  const data = await res.json();   // [{fno, fname}, ...] 가정
+  const tbody = document.querySelector('#wishlistBox');
 
+  let rows = '';
+  for (const w of data) {
+    rows += `
+      <tr>
+        <td>
+          <button type="button" onclick="wishlistDeleteOne(${w.fno})">삭제</button>
+        </td>
+        <td>${w.fname}</td>
+      </tr>
+    `;
+  }
+  tbody.innerHTML = rows || '<tr><td colspan="2">즐겨찾기가 없습니다.</td></tr>';
+};
+
+// // 단일 항목 삭제 → 다시 렌더
+// const wishlistDeleteOne = async (fno) => {
+//   await fetch('/member/wishlist/delete', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     // 서버가 ids 배열을 받는다고 가정. (만약 fno 단일로 받으면 { fno } 로 바꾸세요)
+//     body: JSON.stringify({ ids: [fno] })
+//   });
+//   wishlistRenderRows();
+// };
+
+// // 페이지 진입 시 표 형태로 다시 그려주기
+// document.addEventListener('DOMContentLoaded', wishlistRenderRows);
 
---
+++ src/main/webapp/Members/pwdupdate.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+
+    <jsp:include page="/header.jsp"></jsp:include>
+
+    <div id="container">
+        <h3> 비밀번호 수정 페이지 </h3>
+    </div>
+
+
+
+    <jsp:include page="/footer.jsp"></jsp:include>
+    
+</body>
+</html>
