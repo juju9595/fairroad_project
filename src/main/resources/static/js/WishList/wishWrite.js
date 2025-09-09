414152693a0ef56480355e305e43a11f89749599
leechanyeong <yahn10@naver.com>
2025-09-09 21:19:50 +0900

    77_이찬영_wishWrite.js_비회원즐겨찾기할시문구구성

--- src/main/resources/static/js/WishList/wishWrite.js
+++ src/main/resources/static/js/WishList/wishWrite.js
@@ -15,11 +15,13 @@ const wishWrite = async () => {
         const response = await fetch(`/wish/write?fno=${wish}`,option);
         const data = await response.json();
         console.log(data);
-        if (data > 0) {
-            alert("즐겨찾기 등록 성공");
-        } else {
+        if (data === 0) {
+            alert("회원만 즐겨찾기 가능합니다.");
+        } else if (data === -1) {
             alert("즐겨찾기 취소");
-        }//if end
+        } else if (data > 0) {
+            alert("즐겨찾기 등록 성공");
+        }
     }catch(e){console.log(e)}//catch end
     
     
